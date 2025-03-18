610a9c412b398015940823c4f829fecd4fc025d0
neskam91 <maksenmouhoupro@gmail.com>
2025-03-18 17:35:25 +0100

    Modification de la classe service

--- CIUP/CIUP.java
+++ CIUP/CIUP.java
@@ -4,36 +4,25 @@ import java.util.ArrayList;
 
 public class CIUP {
 
-    private ArrayList<Maison> listeMaison;
-    private ArrayList<Service> listeService;
+	private ArrayList<Maison> listeMaison = new ArrayList<>();
+	private ArrayList<Service> listeService = new ArrayList<>();
 
-    // Constructeur
-    public CIUP() {
-        listeMaison = new ArrayList<>();
-        listeService = new ArrayList<>();
-    }
+	public void afficheListeMaison() {
+		for (Maison maison : listeMaison) {
+			maison.afficheMaison();
+		}
+	}
+	
+	public void afficheListeService() {
+		for (Service service : listeService) {
+			service.afficheService();
+		}
+	}
 
-    // Méthode pour afficher la liste des maisons
-    public void afficheListeMaison() {
-        for (Maison maison : listeMaison) {
-            maison.afficheDesc();
-        }
-    }
-
-    // Méthode pour afficher la liste des services
-    public void afficheListeService() {
-        for (Service service : listeService) {
-            System.out.println("Service: " + service.getNom());
-        }
-    }
-
-    // Ajouter une maison à la liste
-    public void ajouterMaison(Maison maison) {
-        listeMaison.add(maison);
-    }
-
-    // Ajouter un service à la liste
-    public void ajouterService(Service service) {
-        listeService.add(service);
-    }
+	public void ajouterMaison(Maison maison) {
+		listeMaison.add(maison);
+	}
+	public void ajouterService(Service service) {
+		listeService.add(service);
+	}
 }

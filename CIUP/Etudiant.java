5e17f4dbd0a03154083d087a42c081bb64e6c278
Flavio Zamperlini <flavio.zamperlini@universite-paris-saclay.fr>
2025-03-18 14:35:12 +0100

    Add maisonActuelle attribute afficheInfoResume method et le constructeur

--- CIUP/Etudiant.java
+++ CIUP/Etudiant.java
@@ -14,8 +14,24 @@ public class Etudiant {
 	private String universite;
 	private String pieceIdentite;
 	private boolean actuEtudiant;
+	Maison maisonActuelle;
 
-	
+
+	public Etudiant(int num, String nom, String prenom, String adresse, String nationalite, String dateNaissance, String tel, String email, String promotion, String universite, String pieceIdentite) {
+	    this.num = num;
+	    this.nom = nom;
+	    this.prenom = prenom;
+	    this.adresse = adresse;
+	    this.nationalite = nationalite;
+	    this.dateNaissance = dateNaissance;
+	    this.tel = tel;
+	    this.email = email;
+	    this.promotion = promotion;
+	    this.universite = universite;
+	    this.pieceIdentite = pieceIdentite;
+	    this.actuEtudiant = true; // l'etudiant est actuellement dans sa periode d'etude
+	    this.maisonActuelle = null; // l'etudiant n'est pas encore affecte a une maison
+	}
 
 	public String to_String()
 	{
@@ -30,12 +46,15 @@ public class Etudiant {
 		s += " ; " + email;
 		s += " ; " + promotion;
 		s += " ; " + universite;
-		s += " ; " + pieceIdentite;
 		return s;
 	}
 	public void afficheInfo() {
 		System.out.println(to_String());
 	}
-	
+
+	public void afficheInfoResume() {
+		System.out.println(num + " ; " + nom + " ; " + prenom + " ; " + promotion + " ; " + universite);
+	}
+
 
 }

ac4b847813935a51fa875c9bb7464e5f7c69303f
neskam91 <maksenmouhoupro@gmail.com>
2025-03-18 17:28:38 +0100

    Commit finale de la branche service

--- CIUP/CIUP.java
+++ CIUP/CIUP.java
@@ -1,18 +1,39 @@
 package CIUP;
 
+import java.util.ArrayList;
+
 public class CIUP {
 
-	private Maison listeMaison;
-	private Service listeService;
+    private ArrayList<Maison> listeMaison;
+    private ArrayList<Service> listeService;
 
-	public void afficheListeMaison() {
-		// TODO - implement CIUP.afficheListeMaison
-		throw new UnsupportedOperationException();
-	}
+    // Constructeur
+    public CIUP() {
+        listeMaison = new ArrayList<>();
+        listeService = new ArrayList<>();
+    }
 
-	public void afficheListeService() {
-		// TODO - implement CIUP.afficheListeService
-		throw new UnsupportedOperationException();
-	}
+    // Méthode pour afficher la liste des maisons
+    public void afficheListeMaison() {
+        for (Maison maison : listeMaison) {
+            maison.afficheDesc();
+        }
+    }
 
+    // Méthode pour afficher la liste des services
+    public void afficheListeService() {
+        for (Service service : listeService) {
+            System.out.println("Service: " + service.getNom());
+        }
+    }
+
+    // Ajouter une maison à la liste
+    public void ajouterMaison(Maison maison) {
+        listeMaison.add(maison);
+    }
+
+    // Ajouter un service à la liste
+    public void ajouterService(Service service) {
+        listeService.add(service);
+    }
 }

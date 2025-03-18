85404b8c58c7d8198d2cece69a86d0cee0dd0a0b
Yavuz <yavuz.urk28@gmail.com>
2025-03-18 16:17:39 +0100

    Ajouts Des methodes

--- CIUP/FactoryCIUP.java
+++ CIUP/FactoryCIUP.java
@@ -1,30 +1,45 @@
 package CIUP;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FactoryCIUP {
 
-	Etudiant sesEtudiants;
-	Service sesServices;
-	MaisonInternationale saMaison;
-	private String listeMaison;
+    private List<Maison> listeMaisons;  // Liste des maisons
+    private List<Etudiant> listeEtudiants;  // Liste des étudiants
+    private List<Service> listeServices;  // Liste des services
 
-	public void creeMaison() {
-		// TODO - implement FactoryCIUP.creeMaison
-		throw new UnsupportedOperationException();
+    public FactoryCIUP() {
+        // Initialisation des listes
+        this.listeMaisons = new ArrayList<>();
+        this.listeEtudiants = new ArrayList<>();
+        this.listeServices = new ArrayList<>();
+    }
+
+    // Créer une maisonClassique
+    public void creeMaison(String nom, String nationalite, String directeur, String localisationGPS, int nbChambres) {
+        Maison maison = new MaisonClassique(nom, nationalite, directeur, localisationGPS, nbChambres);
+        listeMaisons.add(maison);
 	}
 
-	public void creeService() {
-		// TODO - implement FactoryCIUP.creeService
-		throw new UnsupportedOperationException();
-	}
+    // Créer une maisoninter
+    public void creeMaisonInternationale(String nom, String nationalite, String directeur, String localisationGPS, int nbChambres) {
+        MaisonInternationale maison = new MaisonInternationale(nom, nationalite, directeur, localisationGPS, nbChambres);
+        listeMaisons.add(maison);
+     
+    }
 
-	public void creeEtudiant() {
-		// TODO - implement FactoryCIUP.creeEtudiant
-		throw new UnsupportedOperationException();
-	}
+    // Créer un service 
+    public void creeService(String nomService, MaisonInternationale maison) {
+        Service service = new Service(nomService);
+        maison.ajouterService(service);
+        listeServices.add(service);
+       
+    }
 
-	public void supprMaison() {
-		// TODO - implement FactoryCIUP.supprMaison
-		throw new UnsupportedOperationException();
-	}
-
-}
+    // Créer un étudiant
+    public void creeEtudiant(String nom, String nationalite) {
+        Etudiant etudiant = new Etudiant(nom, nationalite);
+        listeEtudiants.add(etudiant);
+      
+    }
